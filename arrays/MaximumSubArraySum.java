f623a459aa03c72aba961b7355fc6ff715e7a4c3
JACOB B STEPHEN <jacobstebyron@gmail.com>
2025-07-24 16:48:36 +0530

    Arrays Day 11

--- arrays/MaximumSubArraySum.java
+++ arrays/MaximumSubArraySum.java
@@ -3,11 +3,23 @@ public class MaximumSubArraySum {
         int maxSum = Integer.MIN_VALUE;
         int sum = 0;
         int n = nums.length;
+        int ansStart = -1, ansEnd = -1;
+
         for(int i = 0; i < n; i++){
+            if(sum == 0) ansStart = i;
             sum = sum + nums[i];
-            if(maxSum < sum) maxSum = sum;
+            if(maxSum < sum){
+                maxSum = sum;
+                ansEnd = i;
+                
+            }
             if(sum < 0) sum = 0;
         }
+
+        for(int i = ansStart; i <= ansEnd; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
         return maxSum;
 
     }
